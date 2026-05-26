fb2a18d9d836fbb4b80fa6bc126897cb71f52863
nyny567567 <ny567@naver.com>
2026-05-26 12:57:02 +0900

    2026.05.26

---
+++ src/pack/PackageMain2.java
@@ -0,0 +1,10 @@
+package pack;
+
+import pack.a.*;
+
+public class PackageMain2 {
+    static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+    }
+}
---
+++ src/pack/PackageMain3.java
@@ -0,0 +1,11 @@
+package pack;
+
+import pack.a.User;
+
+public class PackageMain3 {
+    static void main(String[] args) {
+        Data data = new Data();
+        User userA = new User();
+        pack.b.User userB = new pack.b.User();
+    }
+}
