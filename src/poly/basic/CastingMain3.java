6b9f7f2398e45f29d861cb335b1f40989465cd77
nyny567567 <ny567@naver.com>
2026-06-06 14:13:28 +0900

    2026.06.06

---
+++ src/poly/basic/CastingMain3.java
@@ -0,0 +1,12 @@
+package poly.basic;
+//upccasting vs downcasting
+public class CastingMain3 {
+    static void main(String[] args) {
+        Child child = new Child();
+        Parent parent1 = (Parent) child; //업캐스팅은 생략 가능, 생략 권장
+        Parent parent2 = child; //업캐스팅 생략
+
+        parent1.parentMethod();
+        parent2.parentMethod();
+    }
+}
