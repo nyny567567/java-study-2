66c6e60dbb0b428134d13fe64832485c61d773d9
nyny567567 <ny567@naver.com>
2026-06-12 13:46:52 +0900

    2026.06.12

---
+++ src/poly/diamond/DiamondMain.java
@@ -0,0 +1,13 @@
+package poly.diamond;
+
+public class DiamondMain {
+    static void main(String[] args) {
+        InterfaceA a = new Child();
+        a.methodA();
+        a.methodCommon();
+
+        InterfaceB b = new Child();
+        b.methodB();
+        b.methodCommon();
+    }
+}
